bb5d8c218a0b7ec87941933a6cf7e86d2323fa7f
nin-jin <sairi-na-tenshi@ya.ru>
2024-04-21 20:16:49 +0000

    layout

---
+++ app/app.view.css.ts
@@ -0,0 +1,22 @@
+namespace $.$$ {
+	
+	$mol_style_define( $hyoo_survey_app, {
+
+		'--mol_theme_hue': '275deg',
+		
+		Menu: {
+			flex: {
+				basis: `15rem`,
+			},
+		},
+
+		Placeholder: {
+			flex: {
+				basis: `15rem`,
+				grow: 0,
+			},
+		},
+		
+	} )
+	
+}
--- meet/form/form.view.css.ts
+++ meet/form/form.view.css.ts
@@ -1,6 +1,18 @@
 namespace $.$$ {
 	
 	$mol_style_define( $hyoo_survey_meet_form, {
+
+		flex: {
+			basis: `40rem`,
+		},
+
+		margin: [ 0, 'auto' ],
+
+		Tools: {
+			flex: {
+				grow: 0,
+			},
+		},
 		
 		Opinion: {
 			background: {

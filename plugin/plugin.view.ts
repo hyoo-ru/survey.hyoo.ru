a53b928cfb7f17dca6eca56120383ad0173fa8c3
Lyumih <m.ivanov1225@gmail.com>
2024-05-11 22:10:35 +0300

    add plugin

---
+++ plugin/plugin.view.ts
@@ -0,0 +1,7 @@
+namespace $.$$ {
+	export class $hyoo_survey_plugin extends $.$hyoo_survey_plugin {
+		meet() {
+			return this.realm().Node( $hyoo_crus_ref( this.id() ), $hyoo_survey_meet )
+		}
+	}
+}

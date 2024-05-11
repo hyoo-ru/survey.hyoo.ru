a0aa84ecc99afd0105b66e5cb95a727bf4481a04
Lyumih <m.ivanov1225@gmail.com>
2024-05-11 23:08:50 +0300

    plugin->embed

--- embed/embed.view.ts
+++ embed/embed.view.ts
@@ -1,5 +1,5 @@
 namespace $.$$ {
-	export class $hyoo_survey_plugin extends $.$hyoo_survey_plugin {
+	export class $hyoo_survey_embed extends $.$hyoo_survey_embed {
 		meet() {
 			return this.realm().Node( $hyoo_crus_ref( this.id() ), $hyoo_survey_meet )
 		}
